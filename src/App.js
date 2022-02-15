c90a401762f971c7ebfc2a3f406c13fd9c5e6fed
“povedaaqui” <Luis.poveda93@hotmail.com>
2022-02-15 22:59:36 +0100

    wallet connected

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import contract from './contracts/MyNFT.json';
 
@@ -7,8 +7,34 @@ const abi = contract.abi;
 
 function App() {
 
-  const checkWalletIsConnected = () => { }
-  const connectWalletHandler = () => { }
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  const checkWalletIsConnected = async () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      console.log("Make sure you have Metamask installed");
+      return;
+    } else {
+      console.log("Wallet exists! We're ready to go")
+    }
+  }
+  
+  const connectWalletHandler = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert("Please install Metamask!");
+    }
+
+    try {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      console.log("Found an account! Address: ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   const mintNftHandler = () => { }
   const connectWalletButton = () => {
     return (
